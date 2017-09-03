815d8b965ee709552f1bffbdd48b0d6e5c8f2615
Graeme Pirie <graeme@graemepirie.com>
2017-09-03 01:55:34 +0100

    Brand colours added. Homepage intro extended full width.

--- wp-content/themes/gp17/blocks/block-text.php
+++ wp-content/themes/gp17/blocks/block-text.php
@@ -1,4 +1,11 @@
-<section class="o-block o-block--text" <?php echo ( get_sub_field( 'background_colour' ) ? 'style="background-color: '. get_sub_field( 'background_colour' ) .';"' : '' ) ?>>
+<style>
+	.o-block--text {
+		border-left: 600rem solid <?php the_sub_field( 'background_colour' );?>;
+		border-right: 600rem solid <?php the_sub_field( 'background_colour' );?>;
+	}
+</style>
+
+<section class="o-block o-block--text u-overflow u-centre-text" <?php echo ( get_sub_field( 'background_colour' ) ? 'style="background-color: '. get_sub_field( 'background_colour' ) .';"' : '' ) ?>>
 	<?php
 
 		if( false === empty( the_sub_field( 'text' ) ) ) {
