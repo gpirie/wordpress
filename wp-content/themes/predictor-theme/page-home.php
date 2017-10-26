72dc4877c44455289d37bca106f7bdc8e5a344ad
Graeme Pirie <graeme@graemepirie.com>
2017-10-27 00:14:36 +0100

    Home Page
    Added home page template. Removed grid css.

---
+++ wp-content/themes/predictor-theme/page-home.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	 * The main template file. 
+	 *
+	 * @link https://codex.wordpress.org/Template_Hierarchy
+	 *
+	 * @package Predictor Theme
+	 *
+	 * Template Name: Home
+	 *
+	 */
+
+	get_header(); 
+
+	echo '<main class="o-maincontent u-clear" itemprop="mainContentOfPage">';
+
+	while ( have_posts() ) : the_post(); 
+
+		get_template_part( 'template-parts/content', 'home' ); 
+
+	endwhile;
+
+	echo '</main>';
+
+	get_sidebar();
+	
+	get_footer();
---
+++ wp-content/themes/predictor-theme/template-parts/content-home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Predictor Theme
+ * 
+ */
+
+?>
+
+<article <?php post_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+	<div class="o-page__content" itemprop="text articleBody">
+
+		<?php the_content(); ?>
+
+	</div>
+
+	<footer class="o-page__footer u-clear u-hidden@print">
+		
+	</footer>	
+
+</article>
