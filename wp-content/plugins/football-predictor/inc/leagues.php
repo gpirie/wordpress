7ffd3c71264929a5b63eb28532beb308816d6920
Graeme Pirie <gpirie@dcthomson.co.uk>
2017-11-09 11:42:21 +0000

    Added function to create custom leagues within the app.

--- wp-content/plugins/football-predictor/inc/leagues.php
+++ wp-content/plugins/football-predictor/inc/leagues.php
@@ -1,12 +1,11 @@
 <?php
     defined( 'ABSPATH' ) or die();
 
-    function predictor_create_league_form() {
-
+    function predictor_create_league_form_display() {
         $league = array();
 
         $html =
-            '<form class="o-form o-form--createleague" action="//'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'" method="post" name="prediction_form">
+            '<form class="o-form o-form--createleague" action="//'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'" method="post" name="createleague_form">
                 <input type="hidden" name="predictor_league_nonce" value="'. wp_create_nonce('predictor-league-security') .'" />
                 <input type="hidden" name="league[league_owner]" value="<?php echo get_current_user_id();?>" />
 
@@ -16,45 +15,64 @@
                 <input class="o-button o-createleague__button" type="submit" value="Create League" />
             </form>';
 
+        return $html;
+    }
+
+    function predictor_create_league_form() {
+        if( is_user_logged_in() ) {
+            $html = predictor_create_league_form_display();
+        }
+        else
+        {
+            $html = '<div class="o-notice o-notice--info">Please log in.</div>';
+        }
+        return $html;
+    }
+
+    function predictor_display_create_league_form_shortcode() {
+        $html = predictor_create_league_form();
 
         return $html;
     }
-    add_shortcode( 'create_league', 'predictor_create_league_form' );
 
-    function predictor_create_league_insert() {
+    function predictor_create_league_form_process() {
         global $wpdb;
 
-        if( isset( $_POST['league'] ) ) {
+        if( ! predictions_form_posted_back( 'predictor_league_nonce' ) ) {
+            return false;
+        }
 
+        if( isset( $_POST['league'] ) ) {
             $wpdb->insert($wpdb->prefix . PREDICTOR_TABLE_LEAGUES,
                 array(
                     'league_name'           => esc_attr( $_POST['league']['league_name'] ),
                     'league_owner'          => get_current_user_id(),
                 )
             );
+
+            return true;
+        }
+        else {
+            return;
         }
     }
 
-    function predictor_create_league_form_process() {
+    function predictor_create_league_form_run() {
 
         $html = '';
         $display_form = true;
 
         // Form submission? Validate and save?
-        if( predictions_form_posted_back( 'predictor_league_nonce' ) ) {
-echo 'boom';
-wp_die();
+        if(predictions_form_posted_back( 'predictor_league_nonce' )) {
             // Validate form!
-            if($errors = prediction_create_league_form_validate()) {
+            if($errors = predictor_league_form_validate()) {
                 $html .= predictions_form_display_errors($errors);
             } else {
-
                 // Save data
-                $save_result = predictor_create_league_insert();
-
+                $save_result = predictor_create_league_form_process();
                 if($save_result) {
-                    $html .= predictor_display_notification( 'Your league has been created.', 'success' );
-                    //$display_form = false;
+                    $html .= predictor_display_notification( 'Thank you for creating a league. Now, share the league code.', 'success' );
+                    $display_form = false;
                 } else {
                     // Display error.
                     $html .= predictions_form_display_errors($errors);
@@ -68,20 +86,16 @@ wp_die();
         }
 
         return $html;
-
     }
+    add_shortcode( 'create_league', 'predictor_create_league_form_run' );
 
-    add_action( 'admin_post_nopriv_predictor_create_league_form_process', 'predictor_create_league_form_process' );
-    add_action( 'admin_post_predictor_create_league_form_process', 'predictor_create_league_form_process' );
-
-    function prediction_create_league_form_validate() {
-
+    function predictor_league_form_validate() {
         if( isset( $_POST['league'] ) ) {
             $errors = array();
 
             $league = $_POST['league'];
 
-            if( empty($_POST['predictor_league_nonce']) || false == wp_verify_nonce($_POST['predictor_league_nonce'], 'predictor-security')) {
+            if( empty($_POST['predictor_league_nonce']) || false == wp_verify_nonce($_POST['predictor_league_nonce'], 'predictor-league-security')) {
                 return ['invalid_nonce' => PREDICTOR_DEFAULT_ERROR];
             }
 
@@ -92,9 +106,10 @@ wp_die();
             return $errors;
         }
 
+        return $errors;
     }
 
-	function prediction_get_user_info( $user_id ) {
+    function prediction_get_user_info( $user_id ) {
 		$user_info = get_userdata( $user_id );
 
 		return $user_info->user_login;
