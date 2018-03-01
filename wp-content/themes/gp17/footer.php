7e84863eb898e552fd0bedd45edb60fa75d7cdd2
Graeme Pirie <graeme@graemepirie.com>
2018-03-01 14:39:15 +0000

    Styled footer area. Moved contact details to last in the order.

--- wp-content/themes/gp17/footer.php
+++ wp-content/themes/gp17/footer.php
@@ -5,25 +5,26 @@
  * @package Theme Name
  */
 
-?>	
+?>
 			<footer class="site-footer u-overflow" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
-				
+
 				<?php
 					gpwd_get_the_custom_logo( 'site-footer' );
 
+					gpwd_navigation( 'footer_menu' );
+
 					gpwd_contact_details();
 
-					gpwd_navigation( 'footer_menu' );
 				?>
 
 			</footer>
 
 			<small class="copyright u-block u-clear">&copy; <?php echo date('Y') . ' ' . ( get_theme_mod( 'copyright' ) ? get_theme_mod( 'copyright' ) : get_bloginfo() );?></small>
-			
+
 			<?php wp_footer();?>
 
 		</div> <!--End site-wrap-->
-	
+
 	</body>
-	
+
 </html>
